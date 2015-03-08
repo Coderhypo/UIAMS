afc625886a67e1fa035746a5303da5a6386a0777
nplus <nplus@localhost.localdomain>
2015-03-08 14:00:00 +0800

    添加蓝本，重构代码

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,35 @@ from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
+from config import config
 
-app = Flask(__name__)
-app.config.from_object("config")
-db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
-
+db = SQLAlchemy()
+bootstrap = Bootstrap()
 login_manager = LoginManager()
-login_manager.setup_app(app)
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'login'
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    
+    db.init_app(app)
+    bootstrap.init_app(app)
+
+    from admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+    
+    from competition import competition as competition_blueprint
+    app.register_blueprint(competition_blueprint, url_prefix='/competition')
+
+    from thesis import thesis as thesis_blueprint
+    app.register_blueprint(thesis_blueprint, url_prefix='/thesis')
+    
+    from patent import patent as patent_blueprint
+    app.register_blueprint(patent_blueprint, url_prefix='/patent')
+
+    login_manager.setup_app(app)
+    login_manager.session_protection = 'strong'
+    login_manager.login_view = 'login'
+
+    return app
+
