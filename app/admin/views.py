2623e962fea9369315bac074d94f3c0c890f3aed
junnplus <junnplus@qq.com>
2015-03-11 19:46:23 +0800

    年级和院系管理的相关route

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,11 +1,47 @@
-#!/usr/bin/env python2
 # coding=utf-8
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, request, jsonify
+from ..models import Acachemy, Grade
 
 from . import admin
 from .. import db
 
 @admin.route('/')
 def index():
-    return 'hello world'
+    return render_template('/admin/index.html')
+
+@admin.route('/grade')
+def acachemy():
+    grades_data = Grade.query.order_by('id').all()
+    return render_template('/admin/grade.html', grades_data = grades_data)
+
+@admin.route('/department')
+def department():
+    departments_data = Acachemy.query.order_by('id').all()
+    return render_template('/admin/department.html', department_data = departments_data)
+
+@admin.route('/major/_remove')
+def majorRemove():
+    id = request.args.get('Id', type=int)
+    print id
+    return jsonify(status=2)
+
+@admin.route('/major/_edit')
+def majorEdit():
+    id = request.args.get('Id', type=int)
+    newName = request.args.get('Name')
+    print id, newName
+    return jsonify(status=2)
+
+@admin.route('/acachemy/_remove')
+def acachemyRemove():
+    id = request.args.get('Id', type=int)
+    print id
+    return jsonify(status=2)
+
+@admin.route('/acachemy/_edit')
+def acachemyEdit():
+    id = request.args.get('Id', type=int)
+    newName = request.args.get('Name')
+    print id, newName
+    return jsonify(status=2)
 
