f600a77a4d1a27a98bfef8f171f077a803b46195
nplus <nplus@localhost.localdomain>
2015-03-27 20:25:11 +0800

    修改数据库改动的代码

--- app/competition/views.py
+++ app/competition/views.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python2
 # coding=utf-8
 from flask import render_template, session, redirect, url_for
+from ..models import CompetitionProject, Grade, Unit, Major
 
 from . import competition
 from .. import db
+import os
+from werkzeug import secure_filename
 
 @competition.route('/individual')
 def individual():
-    return render_template('/competition/individual.html') 
+    competitionProjects = CompetitionProject.query.order_by('id').all()
+    grades = Grade.query.order_by('id').all()
+    acachemys = Unit.query.filter_by(is_acachemy=1).order_by('id').all()
+    majors = Major.query.order_by('id').all()
+    return render_template('/competition/individual.html',competitionProjects=competitionProjects, grades = grades, acachemys = acachemys, majors = majors) 
 
 @competition.route('/team')
 def team():
