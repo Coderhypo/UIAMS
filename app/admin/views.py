65ba7d18ef898bb0d467a5f6c671397f838d703d
nplus <junnplus@qq.com>
2015-04-25 02:21:21 +0800

    取消分页处理

--- app/admin/views.py
+++ app/admin/views.py
@@ -114,7 +114,8 @@ def teacher():
     '''
     teacherRole = Role.query.filter_by(role_name=u'教师').first()
     teachers = User.query.filter_by(role=teacherRole).order_by('id').all()
-    return render_template('/admin/teacher.html',teachers = teachers)
+    return render_template('/admin/teacher.html',teachers = teachers,
+            error_message = None)
 
 @admin.route('/teacher/_get')
 @login_required
@@ -127,6 +128,7 @@ def teacherGet():
 @admin.route('/teacher/_insert', methods=['GET', 'POST'])
 @login_required
 def teacherInsert():
+    error_message = None
     if request.method == 'POST':
         utcnow = datetime.utcnow()
         file = request.files['file']
@@ -137,13 +139,20 @@ def teacherInsert():
             xls = xlrd.open_workbook(file_url)
             table = xls.sheets()[0]
             for i in range(1, table.nrows):
-                print table.row(i)[2].value,table.row(i)[3].value
-                '''
-                teacher_id = table.row(i)[0].value.encode('utf-8')
-                teacher_name = table.row(i)[1].value.encode('utf-8')
-                teacher_unit =
-                '''
-            return redirect(url_for('.teacher'))
+                id = table.row(i)[0].value.encode('utf-8')
+                name = table.row(i)[1].value.encode('utf-8')
+                unit = table.row(i)[2].value.encode('utf-8')
+                try:
+                    teacher = User(id, name)
+                    teacher.role = \
+                        Role.query.filter_by(role_name=u'教师').first()
+                    teacher.password = '123'
+                    teacher.unit = \
+                        Unit.query.filter_by(unit_name = unit).first()
+                    db.session.add(teacher)
+                    db.session.commit()
+                except:
+                    pass
     units = Unit.query.order_by('id').all()
     return render_template('/admin/teacher.html', units=units)
 
@@ -169,12 +178,8 @@ def competition():
 @admin.route('/competition/project')
 @login_required
 def competition_project():
-    project_page = request.args.get('page', 1, type=int)
-    project_pagination = CompetitionProject.query.order_by('id').paginate(
-        project_page,per_page=current_app.config['FLASK_POSTS_PER_PAGE'],error_out=False)
-    projects = project_pagination.items
-    return render_template('/admin/competition_project.html',competitionProjects=projects,
-        project_pagination=project_pagination)
+    projects = CompetitionProject.query.order_by('id').all()
+    return render_template('/admin/competition_project.html',competitionProjects=projects)
 
 @admin.route('/competition/project/_insert', methods=['GET','POST'])
 @login_required
