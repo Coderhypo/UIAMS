f4546b687467b9bd6d695e4129d4242b699a9117
nplus <junnplus@qq.com>
2015-04-28 20:46:29 +0800

    数据库迁移工作

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 from app import create_app, db
 from app.models import User, Role
 from flask.ext.script import Manager, Server, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -12,6 +13,8 @@ def make_shell_context():
 manager.add_command("start", Server(host="0.0.0.0", port=5000, use_debugger=True))
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
-    print 'MANAGE: ', db
     manager.run()
