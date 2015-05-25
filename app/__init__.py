805cc9ae99a89841dfab2ac09929088c79a85941
nplus <junnplus@qq.com>
2015-05-25 11:49:11 +0800

    mv

--- app/__init__.py
+++ app/__init__.py
@@ -18,9 +18,6 @@ with app.app_context():
     db.app = app
     db.init_app(app)
 
-    from main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
     from admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
