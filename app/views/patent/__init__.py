9dbe831ed1d23acd5977bef3f879f2c22fc3bcd0
nplus <junnplus@qq.com>
2015-05-25 11:41:49 +0800

    user views

--- app/views/patent/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-# coding=utf-8
-from flask import Blueprint
-
-patent = Blueprint('patent', __name__)
-
-from . import views
-
--- app/views/thesis/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-# coding=utf-8
-from flask import Blueprint
-
-thesis = Blueprint('thesis', __name__)
-
-from . import views
-
