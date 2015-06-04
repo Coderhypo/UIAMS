210a5e40ab6c861caec1de8f9dfc21c1f41a8fe4
nplus <junnplus@qq.com>
2015-06-04 18:32:16 +0800

    update mixin

--- app/admin/mixin.py
+++ app/admin/mixin.py
@@ -1,19 +1,19 @@
 #-*- coding: utf-8 -*-
 from flask.ext.login import current_user
 from flask.ext.admin.actions import action
+from flask.ext.admin import BaseView
 from flask.ext.admin.contrib.sqla import ModelView
 
-class Mixin(ModelView):
+class ModelViewMixin(ModelView):
     def is_accessible(self):
         return (current_user.is_authenticated()
             and current_user.is_administrator())
-    '''
-    list_template = 'admin/list.html'
-    create_template = 'admin/create.html'
-    edit_template = 'admin/edit.html'
-    '''
 
-    @action('delete', u'删除', u'确定删除选中吗')
-    def action_delete(self, ids):
-        super(Mixin, self).action_delete(ids)
+class BaseViewMixin(BaseView):
+    def is_accessible(self):
+        return (current_user.is_authenticated()
+                and current_user.is_administrator())
 
+    def _handle_view(self, name, **kwargs):
+        if not self.is_accessible():
+            return redirect(url_for('.login', next=request.url))
