18523e454e9ac2d774c85c0a5a6da30db05acced
nplus <junnplus@qq.com>
2015-06-04 18:33:50 +0800

    update

--- app/admin/info.py
+++ app/admin/info.py
@@ -1,25 +1,27 @@
 #-*- coding: utf-8 -*-
 from ..models import Unit
 from ..models import Grade
-from .mixin import Mixin
+from .mixin import ModelViewMixin, BaseViewMixin
+from flask.ext.admin import expose, expose_plugview
 from .. import db
 from . import admin
+from flask import views
 
-class UnitAdmin(Mixin):
+class DepartmentAdmin(ModelViewMixin):
 
     can_restore = False
     can_create = True
-    can_edit = True
+    can_edit = False
     can_delete = True
 
     column_searchable_list = ['unit_name']
 
-    column_list = ('unit_id', 'unit_name')
+    list_template = 'admin/department.html'
 
     def __init__(self, session, **kwargs):
-        super(UnitAdmin, self).__init__(Unit, session, **kwargs)
+        super(DepartmentAdmin, self).__init__(Unit, session, **kwargs)
 
-class GradeAdmin(Mixin):
+class GradeAdmin(ModelViewMixin):
 
     can_restore = False
     can_create = True
@@ -33,5 +35,6 @@ class GradeAdmin(Mixin):
     def __init__(self, session, **kwargs):
         super(GradeAdmin, self).__init__(Grade, session, **kwargs)
 
-admin.add_view(UnitAdmin(db.session, category=u'信息管理'))
-admin.add_view(GradeAdmin(db.session, category=u'信息管理'))
+admin.add_view(DepartmentAdmin(db.session, category=u'信息管理', name=u'院系信息',
+    endpoint='department'))
+admin.add_view(GradeAdmin(db.session, category=u'信息管理', name=u'年级信息'))
