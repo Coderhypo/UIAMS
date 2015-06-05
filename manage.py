d87b89d1a176ab3592c7d9bf07af206f2ac5d56d
nplus <junnplus@qq.com>
2015-06-05 12:09:25 +0800

    deploy db

--- manage.py
+++ manage.py
@@ -15,5 +15,25 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def deploy():
+    from flask.ext.migrate import upgrade
+    from app.models import Role
+
+    # migrate database to latest revision
+    upgrade()
+
+    Role.insert_roles()
+    admin_role = Role.query.filter_by(role_name='管理员').first()
+
+    '''
+    admin = User('admin', 'admin')
+    admin.password = '123'
+    admin.role = admin_role
+
+    db.session.add(admin)
+    db.session.commit()
+    '''
+
 if __name__ == '__main__':
     manager.run()
