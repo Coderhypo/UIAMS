0bffc9834f0d02db2f0f6a82e225751302afe8b2
nplus <junnplus@qq.com>
2015-06-09 19:33:03 +0800

    flash message

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,4 +1,4 @@
-#_*_ coding= UTF-8 _*_
+#-*- coding: utf-8 -*-
 from flask import render_template, session, redirect, url_for, request, flash
 from ..models import User
 from flask.ext.login import login_user, login_required, logout_user
@@ -11,14 +11,15 @@ def login():
         user = User.query.filter_by(user_name = request.form['username']).first()
         if user is not None and user.verify_password(request.form['password']):
             login_user(user, request.form.get('remember_me'))
+            flash(u'您已经成功登录', 'success')
             return redirect('/')
         else:
-            flash('Invalid username or password')
+            flash(u'用户名或密码错误', 'danger')
     return redirect('/')
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash('You have been logged out.')
+    flash('您已经成功退出', 'success')
     return redirect('/')
