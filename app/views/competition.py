caa38395ff50575a049316a752813f297265ae1d
nplus <junnplus@qq.com>
2015-06-12 15:33:09 +0800

    competition upload file and show competition

--- app/views/competition.py
+++ app/views/competition.py
@@ -1,11 +1,16 @@
 # coding=utf-8
-from flask import render_template, session, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, current_app
 from ..models import Project, Grade, Unit, Major, Student, User, Participants, Competition
 from flask.ext.login import login_required
 
 from .. import app, db
 import os
 from werkzeug import secure_filename
+
+ALLOWED_EXTENSIONS = set(['jpg'])
+UPLOAD_FOLDER = app.config['UPLOAD_FOLDER']
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/competition', methods=['GET', 'POST'])
 @login_required
@@ -25,6 +30,18 @@ def competition():
 
         db.session.add(competition)
         db.session.commit()
+
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            file.filename = 'competition_%s.jpg' % competition.id
+            file_url = os.path.join(UPLOAD_FOLDER, file.filename)
+            file.save(file_url)
+
         return render_template('/competition/competition.html', projects =projects)
 
     return render_template('/competition/competition.html', projects =projects)
+
+@app.route('/competition/<int:id>')
+@login_required
+def show_competition(id):
+    return 'hello world'
