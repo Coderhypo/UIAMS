969de4be2338c3c793048eae3334c83d250be9e7
nplus <junnplus@qq.com>
2015-06-12 14:51:06 +0800

    删掉个人竞赛页面视图

--- app/views/competition.py
+++ app/views/competition.py
@@ -7,35 +7,6 @@ from .. import app, db
 import os
 from werkzeug import secure_filename
 
-@app.route('/competition/individual', methods=['GET', 'POST'])
-@login_required
-def individual():
-    competitionProjects = CompetitionProject.query.order_by('id').all()
-    grades = Grade.query.order_by('id').all()
-    if request.method == "POST":
-        student = Student(request.form['student_id'], request.form['student_name'], request.form['grade'], request.form['acachemy'], request.form['major'])
-        db.session.add(student)
-        achievement_name = request.form['achievement_name']
-        winning_level = request.form['winning_level']
-        rate = request.form['rate']
-        winning_time = request.form['winning_time']
-        awards_unit = request.form['awards_unit']
-
-        competition = Competition(achievement_name, winning_level, rate, awards_unit,winning_time)
-        competition.student = student
-        competition.id_competitionproject = request.form['competitionproject']
-        competition.id_teacher_1 = request.form['teacher1']
-        competition.id_teacher_2 = request.form['teacher2']
-
-        db.session.add(competition)
-        db.session.commit()
-
-        return render_template('/competition/individual.html',
-            competitionProjects=competitionProjects, grades = grades)
-
-    return render_template('/competition/individual.html',
-            competitionProjects=competitionProjects, grades = grades)
-
 @app.route('/competition', methods=['GET', 'POST'])
 @login_required
 def competition():
