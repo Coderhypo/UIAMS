bd1344ea6f46480a8c1fc23637b0d7eafd61b1b4
nplus <junnplus@qq.com>
2015-06-17 15:42:25 +0800

    del some excess

--- app/models/grade.py
+++ app/models/grade.py
@@ -11,9 +11,5 @@ class Grade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     grade_name = db.Column(db.String(128), nullable=False, unique=True)
 
-    def __init__(self, id, grade_name):
-        self.id = id
-        self.grade_name = grade_name
-
     def __repr__(self):
         return '<Grade %r>' % self.grade_name
